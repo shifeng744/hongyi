243718554ed9708515125ed7573a309a912f3460
shifeng744 <627060296@qq.com>
2018-02-10 00:29:39 +0800

    第五次

---
+++ js/myvue.js
@@ -0,0 +1,15 @@
+/*pc公共头部*/
+Vue.component('public-header', {
+		template:'<header class="pc-header"><div class="pc-navbar container"><div class="nav-logo"><a href=""><img src="img/logo-white2.png" alt="" /></a>							</div><div class="nav-list"><ul><li><a href="">上海</a></li><li><a href="">立刻找房</a></li><li><a href="">立刻找房</a></li><li><a href="">立刻找房</a></li></ul></div><div class="nav-control"><ul><li><a href="" class="nav-btn-l">伙伴登录</a></li><li><a href="" class="nav-btn-r">用户登录</a></li></ul></div></div></header>'
+	})
+/*mobile公共头部*/
+Vue.component('public-m-header', {
+		template:'<header class="m-header"><a href="" class="mh-l"><img src="img/home.png"/></a><a href="" class="mh-r"><img src="img/con1.jpg" alt="" /></a></header>'
+	})
+
+
+
+
+
+
+
